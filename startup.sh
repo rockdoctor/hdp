4b95723bad5345648885bbda9e1d0d0540f5fb32
rockdoctor <34164718+rockdoctor@users.noreply.github.com>
2018-10-30 14:22:16 -0400

    Add config

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get -y update
+apt-get -y install sudo net-tools
+sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs namenode -format"
+service hadoop start
+service hadoop stop
+service hadoop start
+
+useradd -g hadoop storm
+useradd -g hadoop nifi
+sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user; /usr/local/hadoop/bin/hdfs dfs -mkdir /user/nifi; /usr/local/hadoop/bin/hdfs dfs -chown nifi:hadoop /user/nifi"
+sudo -u hadoop -H sh -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /data; /usr/local/hadoop/bin/hdfs dfs -chown storm:hadoop /data"
+
+sudo -u hadoop -H sh -c "cd; $ZEPPELIN_HOME/bin/zeppelin-daemon.sh start"
